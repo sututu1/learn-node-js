f9528a221c88de127af71b882241cc60ca0153f8
sututu1 <suwenwang29@gmail.com>
2023-03-18 17:46:21 +0800

    匿名函数.js

--- 函数/匿名函数.js
+++ 函数/匿名函数.js
@@ -12,3 +12,24 @@ function fun(){
     console.log("a ="+a);
 }
 fun();
+
+function Person(name , age , gender){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+}
+Person.prototype.toString = function () {
+    return "Person[name="+this.name+",age="+this.age+",gender="+this.gender+"]";
+   //return "我是一个快乐的小Person";
+}
+var per1 = new Person("孙悟空",18,"男");
+var per2 = new Person("猪八戒",18,"男");
+//Person[name=孙悟空,age=18,gender=男]
+
+var result = per.toString();
+//console.log("result =" + result);
+//console.log(per.__proto__.__proto__hasOwnProperty("toString"));
+console.log(per1);
+console.log(per2);
+
+
