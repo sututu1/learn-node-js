c97eacac75edfb0fe61858a1e478d083ce5d9200
sututu1 <suwenwang29@gmail.com>
2023-03-31 09:52:27 +0800

    stack.js

---
+++ 排序/stack.js
@@ -0,0 +1,28 @@
+const stack = [];
+var isValid = function (s){
+    if (s.length % 2 === 1) {
+        return false;
+    }
+    const stack = [];
+    for (let i = 0; i < s.length; i+=1) {
+        const c = s[i];
+        if (c === '(' || c === '{' || c === '[') {
+            stack.pudh(c);
+        } else {
+            const t = stack[stack.length - 1];
+            if (
+                (t === '('&& c === ')') ||
+                (t === '{' && c === '}') ||
+                (t === '[' && c === ']')
+            ) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+}
+
+
+
